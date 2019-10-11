db49b11f874fdb5fffdb6a5ee40763bf9685d42c
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-11 12:14:19 -0400

    Pulled api data, added styling with reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ReactDOM from "react-dom";
+import Characters from "./Components/Characters.js";
+import Card from "./Components/Cards";
 import './App.css';
+import axios from "axios";
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
+    <h1 className="Header">React Wars</h1>
+           <Characters/>
+     </div>
   );
 }
 
 export default App;
+
+
+
+
+    
